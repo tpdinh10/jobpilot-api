855bf2d61c68b53fa6b933bddb252de5e1e748fc
tpdinh10 <tpdinh10@gmail.com>
2026-02-10 16:10:18 -0800

    Improve match scoring by filtering stop words

--- src/services/matchService.js
+++ src/services/matchService.js
@@ -1,9 +1,11 @@
+const stopWords = require("../utils/stopWords");
+
 function normalizeText(text) {
   return text
     .toLowerCase()
     .replace(/[^a-z0-9\s]/g, "")
     .split(/\s+/)
-    .filter(word => word.length > 2);
+    .filter(word => word.length > 2 && !stopWords.has(word));
 }
 
 function getMatchScore(resumeText, jobText) {
---
+++ src/utils/stopWords.js
@@ -0,0 +1,6 @@
+module.exports = new Set([
+  "and", "or", "the", "a", "an", "with", "to", "for",
+  "in", "on", "of", "is", "are", "as", "by", "from",
+  "this", "that", "be", "will", "role", "experience",
+  "years", "year", "using", "use", "required", "preferred"
+]);
